0f464fbc2fbb91b96c46d93797da92ff4b50ebf4
orsoli <fljcoli@gmail.com>
2024-08-27 15:57:52 +0200

    Added  JS structure Steps

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 /* Structure
     - Prepare html Css
+    JS
+    - Retrieve interested elements from DOM
+    - Start 30 seconds timer
+    - Prepare a function to generate random nr
+    - Create a loop to generate 5 random nr and save in randomNumbers array variable already created
+    - Print the randomNumbers in page in random-numbers element
 
-
+    - When time finish, second = 0 stop timer, hide timer field
+    - Create 5 input elemnts
+    - Appand these elements in inputs-form in Dom
+    - Change the message
+    - Show user-field
+    - Create a submit form event 
+    - In this event retrieve the inputs value
+    - Check for each value if is included in rendomNumbers
+    - Prepare result message and print in page in message element
+    - Hidden inputs-form 
+    - Change the button in restart buton
 */
