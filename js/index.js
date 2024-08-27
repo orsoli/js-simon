4bd6682734a34833559d4549fb1b630981fb9fb6
orsoli <fljcoli@gmail.com>
2024-08-27 16:33:56 +0200

    added Preparation phase

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@
     - Retrieve interested elements from DOM
     - Start 30 seconds timer
     - Prepare a function to generate random nr
-    - Create a loop to generate 5 random nr and save in randomNumbers array variable already created
-    - Print the randomNumbers in page in random-numbers element
+    - Create a loop to generate 5 different random nr and save in randomNumbers array variable already created
+    - Print the randomNumbers in page at random-numbers element
 
     - When time finish, second = 0 stop timer, hide timer field
     - Create 5 input elemnts
@@ -19,3 +19,30 @@
     - Hidden inputs-form 
     - Change the button in restart buton
 */
+
+// --- Preparation Phase
+// Retrieve elemnts from DOM
+const messageElement = document.getElementById("message");
+const timerFieldElement = document.querySelector(".timer-field");
+const counterElement = document.querySelector(".counter");
+const randomNumbersElement = document.querySelector(".random-numbers");
+const inputsFormElement = document.getElementById("inputs-form");
+
+// Create variables
+const min = 1; // Minimum number to create random numbers
+const max = 100; // Maximum number to create random numbers
+let second = 30; // The initialized number to countdown
+const generatedNumbers = 5; // Times to generate random numbers
+const randomNumbers = []; // Array to save diferent random numbers
+
+// Define functions
+const onCountDown = () => (counterElement.innerText = --second); // Create a function to decrement the second and print in page
+// Function to generate a rendom nr
+/**
+ *
+ * @param {number} max // From this number
+ * @param {number} min // To this number
+ * @returns {number}
+ */
+const generateRandomNumbers = (max, min) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
